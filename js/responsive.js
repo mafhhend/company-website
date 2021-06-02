0d12fdb171e50a1e85ca06cd9954b41590ffae92
samankhdev <samankh.dev@gmail.com>
2021-06-02 07:39:33 +0430

    responsive:v1

---
+++ js/responsive.js
@@ -0,0 +1,4 @@
+document.querySelector("#toggle-bar").addEventListener("click", () => {
+  document.querySelector("#header-main-menu-bullet-list-right").classList.toggle("toggle-main-menu-right");
+  document.querySelector("#header-main-menu-bullet-list-left").classList.toggle("toggle-main-menu");;
+});
