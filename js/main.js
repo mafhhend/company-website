798a41ba00dfcd7c4361badaa93641777b4902e2
samankhdev <samankh.dev@gmail.com>
2021-06-08 23:35:48 +0430

    fix-slider-bug

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
-let navigationSlides = document.querySelectorAll("#header-slider-navigation ul");
+let navigationSlides = document.querySelectorAll(
+  "#header-slider-navigation ul"
+);
 navigationSlides.forEach((slide) => {
   slide.addEventListener("click", (e) => {
     let SlidePageNumber = e.target.parentElement.getAttribute("data-slide");
@@ -7,31 +9,40 @@ navigationSlides.forEach((slide) => {
       let Items = SlidesItems.querySelectorAll(".item");
       Items.forEach((item) => {
         //   Short-If-Else
-          (item.classList.contains("page-1"))?item.classList.remove("d-none"):item.classList.add("d-none");
+        item.classList.contains("page-1")
+          ? item.classList.remove("d-none")
+          : item.classList.add("d-none");
       });
     }
     if (SlidePageNumber == 2) {
       let Items = SlidesItems.querySelectorAll(".item");
       Items.forEach((item) => {
-                  //   Short-If-Else
-        (item.classList.contains("page-2"))?item.classList.remove("d-none"):item.classList.add("d-none");
+        //   Short-If-Else
+        item.classList.contains("page-2")
+          ? item.classList.remove("d-none")
+          : item.classList.add("d-none");
       });
     }
     if (SlidePageNumber == 3) {
       let Items = SlidesItems.querySelectorAll(".item");
       Items.forEach((item) => {
-          //   Short-If-Else
-        (item.classList.contains("page-3"))?item.classList.remove("d-none"):item.classList.add("d-none");
+        //   Short-If-Else
+        item.classList.contains("page-3")
+          ? item.classList.remove("d-none")
+          : item.classList.add("d-none");
       });
     }
     //   Get All Items Slider Nav
-  let AllItemsSliderNavigation=e.target.parentElement.parentElement.querySelectorAll("li");
-  AllItemsSliderNavigation.forEach((item)=>{
-    //   Remove All Active Class
-    item.querySelector("i").classList.remove("active");
-        if(SlidePageNumber==item.getAttribute("data-slide")){
-            item.querySelector("i").classList.add("active");
-        }
-  });
+    let AllItemsSliderNavigation =
+      e.target.parentElement.parentElement.querySelectorAll("li");
+    // check if you clicked on the Items
+    if (e.target.classList.contains("fa-circle")) {
+      // Remove All Class `Active`
+      AllItemsSliderNavigation.forEach((item) => {
+        item.querySelector("i").classList.remove("active");
+      });
+      // Add Active Class to the new Item
+      e.target.classList.add("active");
+    }
   });
 });
